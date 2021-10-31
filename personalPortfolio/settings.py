4314fa1499cc50c368c66eb96c03719d1039475c
abdulwasih <vasih95@gmail.com>
2021-10-31 10:42:29 +0530

    deployment change1

--- personalPortfolio/settings.py
+++ personalPortfolio/settings.py
@@ -121,14 +121,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static') # add STATIC_ROOT to DIRS
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static') # add STATIC_ROOT to DIRS
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
